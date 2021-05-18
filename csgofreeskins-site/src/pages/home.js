0d6d174e438867e2d15fc05f1bac118776de673d
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-18 21:54:08 +0200

    imported css from Home.css, changed <img> properties

--- csgofreeskins-site/src/pages/home.js
+++ csgofreeskins-site/src/pages/home.js
@@ -1,9 +1,11 @@
 import background from './../images/csgofreeskins.png'
+import "./Home.css"
 
 const Home = () => {
     return (
-        <div>
-            <img src={background}></img>
+        <div className="container">
+            <img src={background} alt="csgofreeskins" className="image"></img><br />
+            <h1>Hello there!</h1>
         </div>
     )
 }
