7b81d9a9df706e82ab6dd86ccb0769178aa76830
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-18 20:36:41 +0200

    changed name from home to Home

--- csgofreeskins-site/src/pages/home.js
+++ csgofreeskins-site/src/pages/home.js
@@ -1,4 +1,4 @@
-const home = () => {
+const Home = () => {
     return (
         <div>
             <h1>Hello there!</h1>
@@ -6,4 +6,4 @@ const home = () => {
     )
 }
 
-export default home
+export default Home
