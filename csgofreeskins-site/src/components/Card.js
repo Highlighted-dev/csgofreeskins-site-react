edc194f437011ccd7ed6bf4767607603f64b493d
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-20 23:06:22 +0200

    Upgraded card sample

--- csgofreeskins-site/src/components/Card.js
+++ csgofreeskins-site/src/components/Card.js
@@ -1,11 +1,19 @@
 import './Card.css'
 
-const Card = () => {
+const Card = ({ title, text, link }) => {
+    
     return (
         <div className="card">
-            <h3>Test</h3>
+            <img src={`/images/${title}.png`} alt={title} width="100%"></img>
+            <div className="text">
+                <h4>{text}</h4>
+            </div>
+            <div className="affilate">
+                <h4><a href={link}>[Go to {title}]</a></h4>
+            </div>
         </div>
     )
 }
+
 
 export default Card
