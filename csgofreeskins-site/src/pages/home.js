f99b6b90f27b47b63bf0a7ec4952549b9e8367d4
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-20 21:24:52 +0200

    Card is now a component in /components/Card.js

--- csgofreeskins-site/src/pages/home.js
+++ csgofreeskins-site/src/pages/home.js
@@ -1,22 +1,15 @@
 import background from './../images/csgofreeskins.png'
 import "./Home.css"
+import Card from './../components/Card'
 
 const Home = () => {
     return (
         <div className="container">
             <img src={background} alt="csgofreeskins" className="image"></img><br />
-            <div className="card">
-                <h3>CSGOROLL</h3>
-            </div>
-            <div className="card">
-                <h3>CSGOROLL</h3>
-            </div>
-            <div className="card">
-                <h3>CSGOROLL</h3>
-            </div>
-            <div className="card">
-                <h3>CSGOROLL</h3>
-            </div>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
         </div>
     )
 }
