3339c02a75c3c663af373060661af4bc89e38893
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-21 20:41:32 +0200

    added 3 new cards

--- csgofreeskins-site/src/pages/home.js
+++ csgofreeskins-site/src/pages/home.js
@@ -1,15 +1,23 @@
-import background from './../images/csgofreeskins.png'
 import "./Home.css"
 import Card from './../components/Card'
 
 const Home = () => {
     return (
         <div className="container">
-            <img src={background} alt="csgofreeskins" className="image"></img><br />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <img src="/images/csgofreeskins.png" alt="csgofreeskins" className="image"></img><br />
+            <Card title="keydrop"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://key-drop.com/en/R/csgofreeskins2"
+            />
+            <Card title="csgoroll"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://csgoroll.com/r/CSGOFREESKINS2" />
+            <Card title="csgoempire"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://csgoempire.com/r/csgofreeskins2" />
+            <Card title="hellcase"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://hellcase.com/fcsgofreeskins2"/>
         </div>
     )
 }
