7cb3410cade40aa8f6ca7069a4dcd440d078b43c
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-21 21:38:01 +0200

    added a lot of new websites

--- csgofreeskins-site/src/pages/home.js
+++ csgofreeskins-site/src/pages/home.js
@@ -8,16 +8,73 @@ const Home = () => {
             <Card title="keydrop"
                 text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
                 link="https://key-drop.com/en/R/csgofreeskins2"
+                code="csgofreeskins2"
             />
             <Card title="csgoroll"
                 text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
-                link="https://csgoroll.com/r/CSGOFREESKINS2" />
+                link="https://csgoroll.com/r/CSGOFREESKINS2"
+                code="csgofreeskins2"
+            />
             <Card title="csgoempire"
                 text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
-                link="https://csgoempire.com/r/csgofreeskins2" />
+                link="https://csgoempire.com/r/csgofreeskins2"
+                code="csgofreeskins2"
+            />
             <Card title="hellcase"
                 text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
-                link="https://hellcase.com/fcsgofreeskins2"/>
+                link="https://hellcase.com/fcsgofreeskins2"
+                code="csgofreeskins2"
+            />
+            <Card title="csgo500"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://csgo500.com/r/csgofreeskins2"
+                code="csgofreeskins2"
+            />
+            <Card title="gamdom"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://gamdom.com/r/csgofreeskins"
+                code="csgofreeskins"
+            />
+            <Card title="datdrop"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://datdrop.com/p/csgofreeskins2"
+                code="csgofreeskins2"
+            />
+            <Card title="daddyskins"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://daddyskins.com/promo-code/csgofreeskins2"
+                code="csgofreeskins2"
+            />
+            <Card title="csgofast"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://csgofast.com/#r/e2r15h-csgo"
+                code="e2r15h-csgo"
+            />
+            <Card title="csgolive"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://www.csgolive.com/affiliates"
+                code="csgofreeskins2"
+            />
+            <Card title="rollbit"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://rollbit.com/referral/csgofreeskins2"
+                code="csgofreeskins2"
+            />
+            <Card title="duelbits"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://duelbits.com/?a=csgofreeskin"
+                code="csgofreeskin"
+            />
+            <Card title="farmskins"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://farmskins.com/ref-csgofree2"
+                code="csgofree"
+            />
+            <Card title="csgoatse"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                link="https://csgoatse.com/#ref=csgofreeskins2"
+                code="csgofreeskins2"
+            />
         </div>
     )
 }
