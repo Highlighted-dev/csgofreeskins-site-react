7369d79e15d703b45f4b711b1def769caa5f3d1d
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-21 21:52:00 +0200

    Created footer

---
+++ csgofreeskins-site/src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h5>Made by Highlighted-dev © 2021. All rights reserverd.</h5>
+        </div>
+    )
+}
+
+export default Footer
