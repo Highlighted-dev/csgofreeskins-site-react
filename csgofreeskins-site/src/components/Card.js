b1350ba7a5c9552407baefe5421b50a7a358b6b1
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-21 20:41:19 +0200

    Add class name to image

--- csgofreeskins-site/src/components/Card.js
+++ csgofreeskins-site/src/components/Card.js
@@ -4,12 +4,12 @@ const Card = ({ title, text, link }) => {
     
     return (
         <div className="card">
-            <img src={`/images/${title}.png`} alt={title} width="100%"></img>
+            <img src={`/images/${title}.png`} alt={title} className="images"></img>
             <div className="text">
                 <h4>{text}</h4>
             </div>
             <div className="affilate">
-                <h4><a href={link}>[Go to {title}]</a></h4>
+                <h4><a href={link}>CODE: CSGOFREESKINS2</a></h4>
             </div>
         </div>
     )
