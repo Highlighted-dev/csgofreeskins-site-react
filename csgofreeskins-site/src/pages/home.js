4762fb3239d4e6c6f55178ad8149d4ee7c0f1f05
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-22 18:29:35 +0200

    changed description for every card, deleted farmskins and added wtfskins and csgopolygon

--- csgofreeskins-site/src/pages/home.js
+++ csgofreeskins-site/src/pages/home.js
@@ -7,74 +7,79 @@ const Home = () => {
         <div className="container">
             <img src="/images/csgofreeskins.png" alt="csgofreeskins" className="image"></img><br />
             <Card title="keydrop"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
-                link="https://key-drop.com/en/R/csgofreeskins2"
-                code="csgofreeskins2"
+                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. From our code you will get 0.55$ free.'
+                link="https://key-drop.com?code=CSGOFREESKIN2"
+                code="csgofreeskin2"
             />
             <Card title="csgoroll"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='Roulette, crash, dice, coinflip and case opening! They also have giveways one a while. Using our code, you will get 3 free cases, from which you can get 0.05$-1000$/case '
                 link="https://csgoroll.com/r/CSGOFREESKINS2"
                 code="csgofreeskins2"
             />
             <Card title="csgoempire"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='Do you like Esport? Here you can bet matches from every popular game like cs:go, lol, dota2 and more!). CSGOEMPIRE also has nice roulette and coinflip.'
                 link="https://csgoempire.com/r/csgofreeskins2"
                 code="csgofreeskins2"
             />
             <Card title="hellcase"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='You have for sure heard about this one. Most popular site with a lot of cases to open, so what are you waiting for? Our code will give you 0.35$ for free.'
                 link="https://hellcase.com/fcsgofreeskins2"
                 code="csgofreeskins2"
             />
             <Card title="csgo500"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='Bored of normal roullete? Here, instead of roullete, you can play a gamemdoe named Wheel, when you can win up to x50. CSGOFREESKINS2 to get free 500 bucks(0.5$).'
                 link="https://csgo500.com/r/csgofreeskins2"
                 code="csgofreeskins2"
             />
             <Card title="gamdom"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='Great casino and match betting(Esport and other sports like football, voleyball etc.). Our code will give you 3 free cases, from which you can get 0.1-35$.'
                 link="https://gamdom.com/r/csgofreeskins"
                 code="csgofreeskins"
             />
             <Card title="datdrop"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text="Do you like case battles? Here you dont have as much cases as on hellcase, but the chances to win something ncie are a little bit higher. CSGOFREESKINS2 for +5% to deposit."
                 link="https://datdrop.com/p/csgofreeskins2"
                 code="csgofreeskins2"
             />
             <Card title="daddyskins"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='Another case opening site. Simlar to hellcase, but here you have arenas instead of case battles, where you can open some free cases. CSGOFREESKINS2 for +5% to deposit.'
                 link="https://daddyskins.com/promo-code/csgofreeskins2"
                 code="csgofreeskins2"
             />
             <Card title="csgofast"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='The oldest site here. You have a lot of unique gamemodes like "fast". This site also offers you a free faucet every 15 minutes (0.01$-0.03$). e2r15h-csgo code will give you 0.3$'
                 link="https://csgofast.com/#r/e2r15h-csgo"
                 code="e2r15h-csgo"
             />
             <Card title="csgolive"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='Case opening site from creators of WTFskins where you can open cases made by players or create your own case. Use our code to get some $ for great start.'
                 link="https://www.csgolive.com/affiliates"
                 code="csgofreeskins2"
             />
             <Card title="rollbit"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='This site features crypto casino and "trading", where you can get up to x1000 of your balance by predicting crypto price in few seconds. Enter csgofreeskins2 to get 0.5$ free.'
                 link="https://rollbit.com/referral/csgofreeskins2"
                 code="csgofreeskins2"
             />
             <Card title="duelbits"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='Casino, roullete, dice duels(coinflip) and crash. I like this site becouse I can deposit csgo skins and withdraw them in ETH. Use your code to get 0.5$.'
                 link="https://duelbits.com/?a=csgofreeskin"
                 code="csgofreeskin"
             />
-            <Card title="farmskins"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
-                link="https://farmskins.com/ref-csgofree2"
-                code="csgofree"
+            <Card title="csgopolygon"
+                text="The old beauty, csgopolygon. You can play on roullete, crash, dice, coinflip and match betting. Enter code CSGOFREESKINS2 to get 1000 coins."
+                link="https://csgopolygon.com/#"
+                code="csgofreeskins2"
             />
             <Card title="csgoatse"
-                text='Probably the best case-opening site right know. It has about 50 different cases and a daily free chest system, where you can get up to 200 USD. They also have free giveway system.'
+                text='The best thing in this site is that if you achieve level 12, you can join a giveaway for 10000 coins x 10 every couple of hours. You have all standard gamemodes here.'
                 link="https://csgoatse.com/#ref=csgofreeskins2"
                 code="csgofreeskins2"
+            />
+            <Card title="wtfskins"
+                text='Roulette, crash and jackpot. Some giveways and leaderboards. They also have daily free system, where you can get 0.02$ every day. Enter code freeskinsbro for 0.25$'
+                link="https://wtfskins.com/"
+                code="freecoinsbro"
             />
             <Footer />
         </div>
