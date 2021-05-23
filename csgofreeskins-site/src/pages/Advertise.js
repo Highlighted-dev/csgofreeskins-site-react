41194d74a9cb1eff0a16cb25bc965cc1d3eb236e
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-23 22:33:56 +0200

    added simple advertise site

---
+++ csgofreeskins-site/src/pages/Advertise.js
@@ -0,0 +1,11 @@
+import "./Advertise.css"
+
+const Advertise = () => {
+    return (
+        <div className="advertise">
+            <h1>Advertise</h1>
+        </div>
+    )
+}
+
+export default Advertise
