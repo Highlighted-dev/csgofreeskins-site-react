7ae1e48499fb3f26853d806dea06c79779f65111
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-23 22:27:40 +0200

    added routes to all sites

--- csgofreeskins-site/src/App.js
+++ csgofreeskins-site/src/App.js
@@ -1,15 +1,18 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from "./pages/About";
+import Advertise from "./pages/Advertise";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Home />
         <Switch>
-          <Route path='/' />
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/advertise' component={Advertise}/>
         </Switch>
       </Router>
     </div>
