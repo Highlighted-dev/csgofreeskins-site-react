89a116ba421e8f1b6ac993fe528f115b02832a26
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-24 23:12:41 +0200

    Completed advertise website

--- csgofreeskins-site/src/pages/Advertise.js
+++ csgofreeskins-site/src/pages/Advertise.js
@@ -3,7 +3,10 @@ import "./Advertise.css"
 const Advertise = () => {
     return (
         <div className="advertise">
-            <h1>Advertise</h1>
+            <div>
+                <h1>Advertise</h1>
+                <h4>If you want to advertise your website on this site, send a message to this e-mail: <br></br>bartosz.korz@gmail.com</h4>
+            </div>
         </div>
     )
 }
